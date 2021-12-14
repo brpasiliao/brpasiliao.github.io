4f3674333c4be5b5b9361b646eacb3c26c2d2623
brpasiliao <brpasiliao@gmail.com>
2021-12-14 16:41:24 -0500

    improved weather app

--- weather/weather.js
+++ weather/weather.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
   function weather(lat, long) {
     var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
     $.getJSON(URL, function (data) {
+      console.log(data);
       updateDOM(data);
     });
   }
@@ -21,12 +22,22 @@ $(document).ready(function () {
   function updateDOM(data) {
     var city = data.name;
     var temp = Math.round(data.main.temp);
+    var min = Math.round(data.main.temp_min) + "&#176;";
+    var max = Math.round(data.main.temp_max) + "&#176;";
     var desc = data.weather[0].description;
     var icon = data.weather[0].icon;
+    var feel = data.main.feels_like;
+    var humi = data.main.humidity;
+    var pres = data.main.pressure;
 
     $("#city").html(city);
     $("#temp").html(temp);
+    $("#min").html(min);
+    $("#max").html(max);
     $("#desc").html(desc);
     $("#icon").attr("src", icon);
+    $("#feel").html(feel);
+    $("#humi").html(humi);
+    $("#pres").html(pres);
   }
 });
